6a6c0e50cfe87251773add21aa4edbbac6f999a7
AAdrien-3107 <impressbecker@gmail.com>
2021-02-01 23:50:48 +0100

    Update projects data

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-import express from "express";
-import pug from "pug";
-import data from ("./data.json");
+const express = require('express');
+const pug = require('pug');
+const aboutRouter = require("./routes/about");
+//import data from ("./data.json");
 
-var app = express();
-
+const app = express();
+//const pugApp = pug();
 
 
 //middleware setup
@@ -11,18 +12,19 @@ app.set('view engine', 'pug');
 
 //route the static files
 app.use(express.static("public"));
+app.use('/about', aboutRouter)
 
 //setting routes 
-app.get("/", function (req, res){
-    res.send('GET request to the homepage');
+app.get("/", (req, res)=>{
+    res.render('layout');
 });
 app.get("/about", function (req, res){
     res.send('GET request to the homepage');
 });
 
-app.get(`/projects/${id}`, function (req, res){
-    res.send('GET request to the homepage');
-});
+//app.get(`/projects/${id}`, function (req, res){
+//    res.send('GET request to the homepage');
+//});
 
 app.listen(3000);
-console.log("Node server running on port %d", app.address().port);
+//console.log("Node server running on port %d", app.address().port);
